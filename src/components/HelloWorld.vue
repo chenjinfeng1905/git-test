9c422c537e0e682a24fbb70a4c48867d31eadc35
emerald <1101908424@qq.com>
2019-11-14 15:38:53 +0800

    add some info

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <!-- <Countdown endTime="1549960618" :callback="callback" endText="已经结束了"></Countdown> -->
+    <draggable v-model="list2" :move="move">
+      <li v-for="(item, index) in list2" :key="index">{{item}}</li>
+    </draggable>
+  </div>
+</template>
+
+<script>
+
+import Countdown from '@/components/time'
+import draggable from 'vuedraggable'
+
+export default {
+  name: 'HelloWorld',
+
+  components: {
+    Countdown, draggable
+  },
+
+  data () {
+    return {
+      list2: [
+        '张三11111', '里斯222222', '张三33333', '里斯44444'
+      ]
+    }
+  },
+
+  methods: {
+    move: function (originalEvent) {
+      console.log(originalEvent)
+    }
+  }
+}
+</script>
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
